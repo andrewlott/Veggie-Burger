5aa90b0ac42add86d4d9c2c45c78aa9e6e4a4fce
Andrew Lott <AndrewLott@gmail.com>
2011-07-03 01:56:18 -0400

    calling instagram API and presenting it in a 4x4 grid forever

---
+++ JS/instagram.js
@@ -0,0 +1,51 @@
+var id = 'b9c719516ec54867b756ad8a2f6dcd83';
+var secret = '62f7eeac9ebe46f890c74fdfbcc17a7d';
+var base_url = 'https://api.instagram.com/v1/media/popular?client_id=';
+
+num = 16;//$('.box li').length;
+thumbs = [];
+index = 0;
+
+
+//init placeholders
+for (var i=0; i<num; i++)
+    $('.box ul').append('<li><img src=""/></li>');
+
+getImgs(num);
+
+
+
+function getImgs(total){
+$.getJSON(  base_url + id + "&callback=?&count="+total,load);
+}
+
+
+function load(data){
+      $.each(data.data, function(i, obj){
+	  if(!~thumbs.indexOf(obj.images.thumbnail.url))
+	     thumbs.push(obj.images.thumbnail.url);
+      });
+    if(index==0) start();
+};
+
+function start() {
+    //init the box with 
+    swap();
+    //then start off the timer
+    setInterval(swap, 5000);
+}
+
+function swap() {
+    console.log('@swap');
+    $('.box ul').fadeOut('slow', function(){
+	$('.box li img').attr('src', thumbs[index++]);
+    });
+    $('.box ul').fadeIn('slow', function(){
+	if(index % num == 0){
+	    console.log(index);
+	    getImgs(index*Math.floor(index/num.round) );
+	}	
+    });
+
+    
+}
